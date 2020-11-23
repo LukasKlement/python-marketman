b668ebbfa3d989370d3e1d92760dbbd3acda8426
Lukas Klement <lukas.klement@me.com>
2020-11-23 18:02:40 +0100

    Update version number

--- python_marketman/__version__.py
+++ python_marketman/__version__.py
@@ -4,7 +4,7 @@ This file shamelessly taken from the requests library"""
 __title__ = 'python-marketman'
 __description__ = 'A basic Marketman.com REST API client.'
 __url__ = 'https://github.com/LukasKlement/python-marketman'
-__version__ = '0.0.1'
+__version__ = '0.1'
 __author__ = 'Lukas Klement'
 __author_email__ = 'lukas.klement@me.com'
 __license__ = 'Apache 2.0'
